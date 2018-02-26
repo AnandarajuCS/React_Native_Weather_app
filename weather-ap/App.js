2e7b8f9652735ccb1e6c941d4de33209dcc451cb
Anandaraju CS <ana.ceg92@gmail.com>
2018-02-25 20:49:33 -0800

    Added feature to get current location and weather conditions with appropriate message

--- weather-ap/App.js
+++ weather-ap/App.js
@@ -1,18 +1,107 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  Alert,
+  } from 'react-native';
 import Exponent from 'expo';
+import Icon from 'react-native-vector-icons/Ionicons';
+import Hightlight from 'react-native-highlight-words';
+import {fetchWeather} from './utils/weatherAPI';
+
+const iconNames = {
+  Clear: 'md-sunny',
+  Rain: 'md-rainy',
+  Thunderstorm: 'md-thunderstorm',
+  Clouds: 'md-cloudy',
+  Snow: 'md-snow',
+  Drizzle: 'md-umbrella',
+};
+
+const phrases = {
+  Clear: {
+    title: "It's Awesome Amaze Balls",
+    subtitle: "Rock that Shit!",
+    highlight: "Awesome",
+  },
+  Rain: {
+    title: "Rain rain go away",
+    subtitle: "Stay inside and code all day",
+    highlight: "away",
+  },
+  Thunderstorm: {
+    title: "Major Thunderstrike",
+    subtitle: "Unplug those devices!",
+    highlight: "Thunderstrike",
+  },
+  Clouds: {
+    title: "Cloud Storage limit reached",
+    subtitle: "error: 5000 - cirrocumulus",
+    highlight: "limit",
+  },
+  Snow: {
+    title: "Oh God! Brain freeze",
+    subtitle: "You are not supposed to eat it!",
+    highlight: "Freeze",
+  },
+  Drizzle: {
+    title: "I am loving it!",
+    subtitle: "It's great outside",
+    highlight: "loving",
+  },
+};
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      temp: 0,
+      weather: 'Clear',
+    };
+  }
+  // componentWillMount() {
+  //   this.state = {
+  //     temp: 0,
+  //     weather: 'Clear',
+  //   };
+  // }
+  componentDidMount() {
+    this.getLocation();
+    fetchWeather(37, -121).then(res => console.log(res));
+  }
+
+  getLocation() {
+    navigator.geolocation.getCurrentPosition(
+      (posData) => fetchWeather(posData.coords.latitude, posData.coords.longitude)
+        .then(res => {
+          this.setState({
+            temp: res.temp,
+            weather: res.weather,
+          });
+        }),
+      (error) => Alert(error),
+      {timeout:10000}
+    );
+  }
+
   render() {
     return (
       <View style={styles.container}>
+        <StatusBar hidden={true} />
         <View style={styles.header}>
-          <Text>Icon</Text>
-          <Text style= {styles.temp}>24</Text>
+          <Icon name={iconNames[this.state.weather]} size={80} color={'white'}/>
+          <Text style= {styles.temp}>{this.state.temp}</Text>
         </View>
         <View style= {styles.body}>
-          <Text style= {styles.title}>Build a Awesome Weather App!!</Text>
-          <Text style= {styles.subtitle}>Let's make it rain!</Text>
+          <Hightlight
+            style= {styles.title}
+            highlightStyle={{color: 'red'}}
+            searchWords={[phrases[this.state.weather].highlight]}
+            textToHighlight= {phrases[this.state.weather].title}
+          />
+          <Text style= {styles.subtitle}>{phrases[this.state.weather].subtitle}</Text>
         </View>
       </View>
     );
@@ -41,7 +130,7 @@ const styles = StyleSheet.create({
     margin: 10,
   },
   title: {
-    fontSize: 78,
+    fontSize: 70,
     color: 'white',
     marginBottom: 10,
   },
