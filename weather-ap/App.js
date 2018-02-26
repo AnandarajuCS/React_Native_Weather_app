c75c12de35124b20af71b6bf9af2f82185f800b5
Anandaraju CS <ana.ceg92@gmail.com>
2018-02-25 21:14:04 -0800

    Added a default page view while fetching data

--- weather-ap/App.js
+++ weather-ap/App.js
@@ -12,6 +12,7 @@ import Hightlight from 'react-native-highlight-words';
 import {fetchWeather} from './utils/weatherAPI';
 
 const iconNames = {
+  Default: 'md-time',
   Clear: 'md-sunny',
   Rain: 'md-rainy',
   Thunderstorm: 'md-thunderstorm',
@@ -21,44 +22,65 @@ const iconNames = {
 };
 
 const phrases = {
+  Default: {
+    title: "Fetching your Weather data",
+    subtitle: "Please be patient, you are about to see a miracle",
+    highlight: "Weather",
+    color: "#636363",
+    background: "#9C9C9C",
+  },
   Clear: {
     title: "It's Awesome Amaze Balls",
     subtitle: "Rock that Shit!",
     highlight: "Awesome",
+    color: "#E32500",
+    background: "#FFD017",
   },
   Rain: {
     title: "Rain rain go away",
     subtitle: "Stay inside and code all day",
     highlight: "away",
+    color: "#004A96",
+    background: "#2F343A",
   },
   Thunderstorm: {
     title: "Major Thunderstrike",
     subtitle: "Unplug those devices!",
     highlight: "Thunderstrike",
+    color: "#FBFF46",
+    background: "#020202",
   },
   Clouds: {
     title: "Cloud Storage limit reached",
     subtitle: "error: 5000 - cirrocumulus",
     highlight: "limit",
+    color: "#0044FF",
+    background: "#939393",
   },
   Snow: {
     title: "Oh God! Brain freeze",
     subtitle: "You are not supposed to eat it!",
     highlight: "Freeze",
+    color: "#021D4C",
+    background: "#15A678",
   },
   Drizzle: {
     title: "I am loving it!",
     subtitle: "It's great outside",
     highlight: "loving",
+    color: "#B3F6E4",
+    background: "#1FBB68",
   },
 };
 
 export default class App extends React.Component {
+  // to resolve this error
+  // Assigning directly to this.state is deprecated (except inside a component's constructor). Use setState instead.
   constructor(props) {
     super(props);
     this.state = {
       temp: 0,
-      weather: 'Clear',
+      weather: 'Default',
     };
   }
   // componentWillMount() {
@@ -69,7 +91,6 @@ export default class App extends React.Component {
   // }
   componentDidMount() {
     this.getLocation();
-    fetchWeather(37, -121).then(res => console.log(res));
   }
 
   getLocation() {
@@ -77,7 +98,7 @@ export default class App extends React.Component {
       (posData) => fetchWeather(posData.coords.latitude, posData.coords.longitude)
         .then(res => {
           this.setState({
-            temp: res.temp,
+            temp: Math.round(res.temp),
             weather: res.weather,
           });
         }),
@@ -88,16 +109,16 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
+      <View style={[styles.container, {backgroundColor: phrases[this.state.weather].background}]}>
         <StatusBar hidden={true} />
         <View style={styles.header}>
           <Icon name={iconNames[this.state.weather]} size={80} color={'white'}/>
-          <Text style= {styles.temp}>{this.state.temp}</Text>
+          <Text style= {styles.temp}>{this.state.temp} °C</Text>
         </View>
         <View style= {styles.body}>
           <Hightlight
             style= {styles.title}
-            highlightStyle={{color: 'red'}}
+            highlightStyle={{color: phrases[this.state.weather].color}}
             searchWords={[phrases[this.state.weather].highlight]}
             textToHighlight= {phrases[this.state.weather].title}
           />
--- weather-ap/utils/weatherAPI.js
+++ weather-ap/utils/weatherAPI.js
@@ -1,7 +1,7 @@
 const rootUrl = 'http://api.openweathermap.org/data/2.5/weather?appid=6239b7a5e9e5158e36e907023a265ae3';
 
 export const fetchWeather = (lat, lon) => {
-    const url = rootUrl + '&lat=' + lat + '&lon=' + lon;
+    const url = rootUrl + '&lat=' + lat + '&lon=' + lon + '&units=metric';
     console.log(url);
 
     return fetch(url)
